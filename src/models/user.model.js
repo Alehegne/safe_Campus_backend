b2e0c49e1368af3315a88493320085f4b5884b17
Alehegne Geta <alehegngetaenyewuu@gmail.com>
2025-05-05 03:16:06 +0300

    Update user.model.js

--- src/models/user.model.js
+++ src/models/user.model.js
@@ -17,7 +17,6 @@ const userSchema = new mongoose.Schema(
       // for push notifications using firebase cloud messaging
       type: String,
       trim: true,
-      required: true,
     },
     fullName: {
       type: String,
